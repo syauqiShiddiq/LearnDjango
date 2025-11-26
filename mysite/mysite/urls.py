b05e2e176ed3acfa8a273980a1601e69869c2aad
Syauqi <syauqiash95@gmail.com>
2025-11-26 11:52:32 +0700

    Django Tutorial - Create a Polls app complete

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from django.urls import include, path  # <--- Pastikan 'include' ada
-from django.conf import settings       # <--- Tambahkan ini untuk cek mode DEBUG
+from django.urls import include, path  
+from django.conf import settings       
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
 
-# --- KODE DEBUG TOOLBAR (Gunakan format standar ini) ---
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        # Perhatikan kita menggunakan 'include(debug_toolbar.urls)'
-        # BUKAN 'debug_toolbar_urls'
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
